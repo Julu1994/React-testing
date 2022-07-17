beaa166bae1fd2a39ed65857a6a3070f5a661034
julu1994 <mrjewel837@gmail.com>
2022-07-17 02:11:32 +0200

    making the task input field

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
+import Task from "./Components/task";
 function App() {
-    return <div className="App">Creating the new project</div>;
+    return (
+        <div className="App">
+            <Task />
+        </div>
+    );
 }
 
 export default App;
--- src/Components/task.js
+++ src/Components/task.js
@@ -1,7 +1,35 @@
-import React from "react";
+import "./task.css";
+import React, { useState } from "react";
 
-function task() {
-    return <div>task</div>;
+function Task() {
+    const [task, setTask] = useState("");
+    const [taskList, setTaskList] = useState([]);
+    const taskHandler = (event) => {
+        event.preventDefault();
+        const myTask = { task };
+        setTaskList([...taskList, myTask]);
+        console.log(taskList);
+    };
+    return (
+        <div>
+            <form className="form" onSubmit={taskHandler}>
+                <label htmlFor="title"> Title </label>
+
+                <input
+                    className="task-input"
+                    type="text"
+                    value={task}
+                    onChange={(event) => setTask(event.target.value)}
+                />
+                <button type="submit">Save</button>
+            </form>
+            {taskList.map((item) => (
+                <li key={item.task} className="task">
+                    {item.task}
+                </li>
+            ))}
+        </div>
+    );
 }
 
-export default task;
+export default Task;
